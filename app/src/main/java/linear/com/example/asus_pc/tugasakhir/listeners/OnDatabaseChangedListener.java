e9a13c56c54499593dee9840e5c4b9b7cc8c88e0
febryprasasti04 <prasastifebry@gmail.com>
2018-09-28 16:09:17 +0700

    Upload

---
+++ app/src/main/java/linear/com/example/asus_pc/tugasakhir/listeners/OnDatabaseChangedListener.java
@@ -0,0 +1,10 @@
+package linear.com.example.asus_pc.tugasakhir.listeners;
+
+/**
+ * Created by Daniel on 1/3/2015.
+ * Listen for add/rename items in database
+ */
+public interface OnDatabaseChangedListener{
+    void onNewDatabaseEntryAdded();
+    void onDatabaseEntryRenamed();
+}
